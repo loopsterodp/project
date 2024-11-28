c34d1d3733d4bdebd8a75c80bf2681810f84fa8a
loopsterodp <omkardpmain@gmail.com>
2024-11-28 21:10:52 +0530

    Update card.tsx

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
-export const Card = ({ children, className }) => {
-  return (
-    <div className={`card ${className}`}>
-      {children}
-    </div>
-  );
-};
+export const CardHeader = ({ children }) => (
+  <div className="card-header">
+    {children}
+  </div>
+);
+
+export const CardTitle = ({ children }) => (
+  <div className="card-title">
+    {children}
+  </div>
+);
+
+export const CardContent = ({ children }) => (
+  <div className="card-content">
+    {children}
+  </div>
+);
